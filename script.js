a5a5dc4f6b59a9eb9314fea8d07188807b575e64
F4R1xx <ibmecfagundes@gmail.com>
2024-05-07 20:00:01 -0300

    Versao 1.4

--- script.js
+++ script.js
@@ -1,4 +1,92 @@
-const _0x25f6c2=_0x18dd;(function(_0x4f7d6d,_0x5b48f2){const _0x243e2d=_0x18dd,_0x4aafb4=_0x4f7d6d();while(!![]){try{const _0x46d17b=parseInt(_0x243e2d(0xba))/0x1+parseInt(_0x243e2d(0xb7))/0x2+parseInt(_0x243e2d(0xc5))/0x3+parseInt(_0x243e2d(0xa9))/0x4*(-parseInt(_0x243e2d(0xa6))/0x5)+-parseInt(_0x243e2d(0xb4))/0x6+-parseInt(_0x243e2d(0xab))/0x7*(-parseInt(_0x243e2d(0xc3))/0x8)+-parseInt(_0x243e2d(0xb1))/0x9;if(_0x46d17b===_0x5b48f2)break;else _0x4aafb4['push'](_0x4aafb4['shift']());}catch(_0x39d8ec){_0x4aafb4['push'](_0x4aafb4['shift']());}}}(_0x1742,0xc39eb));const _0x1ddd7f=(function(){let _0x5c40d6=!![];return function(_0x273967,_0x177925){const _0x449eae=_0x5c40d6?function(){const _0x359cdb=_0x18dd;if(_0x177925){const _0x4a3ffb=_0x177925[_0x359cdb(0xd2)](_0x273967,arguments);return _0x177925=null,_0x4a3ffb;}}:function(){};return _0x5c40d6=![],_0x449eae;};}()),_0x35c971=_0x1ddd7f(this,function(){const _0x75852b=_0x18dd;return _0x35c971[_0x75852b(0x9f)]()[_0x75852b(0xcf)](_0x75852b(0xae))[_0x75852b(0x9f)]()[_0x75852b(0xad)](_0x35c971)['search'](_0x75852b(0xae));});_0x35c971();const _0x34eeae=(function(){let _0x4c96bc=!![];return function(_0x2aaa7a,_0x35f646){const _0x442a30=_0x4c96bc?function(){if(_0x35f646){const _0x37874e=_0x35f646['apply'](_0x2aaa7a,arguments);return _0x35f646=null,_0x37874e;}}:function(){};return _0x4c96bc=![],_0x442a30;};}()),_0x49011b=_0x34eeae(this,function(){const _0x551ccb=_0x18dd,_0x4c1cfd=function(){const _0x24643e=_0x18dd;let _0x179132;try{_0x179132=Function('return\x20(function()\x20'+_0x24643e(0xc8)+');')();}catch(_0x5adbf5){_0x179132=window;}return _0x179132;},_0x28f590=_0x4c1cfd(),_0xc0a1ff=_0x28f590['console']=_0x28f590[_0x551ccb(0xce)]||{},_0x481f36=['log',_0x551ccb(0xa5),_0x551ccb(0xb9),_0x551ccb(0xb0),_0x551ccb(0xa0),'table','trace'];for(let _0x356a41=0x0;_0x356a41<_0x481f36[_0x551ccb(0xbb)];_0x356a41++){const _0x6626cd=_0x34eeae[_0x551ccb(0xad)][_0x551ccb(0xb8)][_0x551ccb(0x9d)](_0x34eeae),_0x432907=_0x481f36[_0x356a41],_0x28cae2=_0xc0a1ff[_0x432907]||_0x6626cd;_0x6626cd['__proto__']=_0x34eeae['bind'](_0x34eeae),_0x6626cd[_0x551ccb(0x9f)]=_0x28cae2[_0x551ccb(0x9f)]['bind'](_0x28cae2),_0xc0a1ff[_0x432907]=_0x6626cd;}});function _0x18dd(_0x13a9f0,_0x19d67d){const _0x595826=_0x1742();return _0x18dd=function(_0x49011b,_0x34eeae){_0x49011b=_0x49011b-0x9c;let _0x3d5559=_0x595826[_0x49011b];return _0x3d5559;},_0x18dd(_0x13a9f0,_0x19d67d);}_0x49011b();const xhr=new XMLHttpRequest();xhr['open'](_0x25f6c2(0xb3),_0x25f6c2(0xa7),!![]),xhr[_0x25f6c2(0xc7)]=function(){const _0x39c745=_0x25f6c2;if(xhr[_0x39c745(0xc4)]===0x4&&xhr[_0x39c745(0xbe)]===0xc8){const _0x21a8e3=JSON[_0x39c745(0xb2)](xhr[_0x39c745(0xaf)]);createMusicCards(_0x21a8e3[_0x39c745(0xc1)]);}},xhr['send']();function createMusicCards(_0xbca665){const _0x164bee=_0x25f6c2,_0x2b9523=document[_0x164bee(0xcb)](_0x164bee(0xb5));_0x2b9523[_0x164bee(0x9e)]='',_0xbca665[_0x164bee(0xaa)](_0x370d07=>{const _0x13eaf3=_0x164bee,_0x1b83c9=document['createElement']('div');_0x1b83c9[_0x13eaf3(0xc2)][_0x13eaf3(0x9c)](_0x13eaf3(0xcc)),_0x1b83c9[_0x13eaf3(0x9e)]='\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22card\x22\x20style=\x22width:\x2018rem;\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22card-body\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h5\x20class=\x22card-title\x22>'+_0x370d07[_0x13eaf3(0xcd)]+_0x13eaf3(0xc9)+_0x370d07['Trecho']+'</p>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20<ul\x20class=\x22list-group\x20list-group-flush\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22list-group-item\x22><strong>Código:\x20'+_0x370d07[_0x13eaf3(0xb6)]+'</strong></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22list-group-item\x22>Interprete:\x20'+_0x370d07['Interprete']+_0x13eaf3(0xbc)+_0x370d07['Idioma']+_0x13eaf3(0xa3),_0x2b9523[_0x13eaf3(0xa2)](_0x1b83c9);});}function _0x1742(){const _0x20cdc2=['Interprete','apply','includes','add','bind','innerHTML','toString','exception','value','appendChild','</li>\x0a\x20\x20\x20\x20</ul>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20</div>\x0a','Idioma','warn','1940qlKtIu','Musicas.json','img','6824MLLuvM','forEach','5300456JKEqUk','send','constructor','(((.+)+)+)+$','responseText','error','2816082XxrLWj','parse','GET','8711634PYSiTt','music-cards','Codigo','2576174lDLRYa','prototype','info','118441kljBYA','length','</li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22list-group-item\x22>Idioma:\x20','searchInput','status','toLowerCase','BRA','Musicas','classList','16USewrV','readyState','921240KkIoQn','filter','onreadystatechange','{}.constructor(\x22return\x20this\x22)(\x20)','</h5>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22card-text\x22>','EUA','getElementById','card-Musicas','Nome','console','search','open'];_0x1742=function(){return _0x20cdc2;};return _0x1742();}function search(){const _0x55aa99=_0x25f6c2,_0x3b2577=document['getElementById'](_0x55aa99(0xbd))[_0x55aa99(0xa1)][_0x55aa99(0xbf)](),_0x80e62=new XMLHttpRequest();_0x80e62['open'](_0x55aa99(0xb3),_0x55aa99(0xa7),!![]),_0x80e62[_0x55aa99(0xc7)]=function(){const _0x4dd3bd=_0x55aa99;if(_0x80e62[_0x4dd3bd(0xc4)]===0x4&&_0x80e62[_0x4dd3bd(0xbe)]===0xc8){const _0x4ad587=JSON[_0x4dd3bd(0xb2)](_0x80e62['responseText'])[_0x4dd3bd(0xc1)],_0x2dedb9=_0x4ad587[_0x4dd3bd(0xc6)](_0x16fc36=>{const _0x487c06=_0x4dd3bd;return _0x16fc36['Codigo'][_0x487c06(0x9f)]()[_0x487c06(0xd3)](_0x3b2577)||_0x16fc36[_0x487c06(0xcd)][_0x487c06(0xbf)]()[_0x487c06(0xd3)](_0x3b2577)||_0x16fc36[_0x487c06(0xd1)][_0x487c06(0xbf)]()[_0x487c06(0xd3)](_0x3b2577);});createMusicCards(_0x2dedb9);}},_0x80e62[_0x55aa99(0xac)]();}function search_por_autor(_0x3313d9){const _0x510166=_0x25f6c2;_0x3313d9=_0x3313d9[_0x510166(0xbf)]();const _0x31ed1b=new XMLHttpRequest();_0x31ed1b[_0x510166(0xd0)](_0x510166(0xb3),_0x510166(0xa7),!![]),_0x31ed1b['onreadystatechange']=function(){const _0x5bec88=_0x510166;if(_0x31ed1b[_0x5bec88(0xc4)]===0x4&&_0x31ed1b['status']===0xc8){const _0x1703e8=JSON[_0x5bec88(0xb2)](_0x31ed1b['responseText'])[_0x5bec88(0xc1)],_0x36ecad=_0x1703e8['filter'](_0x2be4a4=>{const _0x25fe14=_0x5bec88;return _0x2be4a4[_0x25fe14(0xb6)][_0x25fe14(0x9f)]()[_0x25fe14(0xd3)](_0x3313d9)||_0x2be4a4['Nome'][_0x25fe14(0xbf)]()[_0x25fe14(0xd3)](_0x3313d9)||_0x2be4a4[_0x25fe14(0xd1)][_0x25fe14(0xbf)]()['includes'](_0x3313d9)||_0x2be4a4[_0x25fe14(0xa4)][_0x25fe14(0xbf)]()[_0x25fe14(0xd3)](_0x3313d9);});createMusicCards(_0x36ecad);}},_0x31ed1b['send']();}let isBRA=!![];const image_pais=document[_0x25f6c2(0xcb)](_0x25f6c2(0xa8));function toggleFunction(){const _0x23c3ee=_0x25f6c2;isBRA?search_por_autor(_0x23c3ee(0xca)):search_por_autor(_0x23c3ee(0xc0)),isBRA=!isBRA,updateButtonColor();}
+// Requisição AJAX para carregar o arquivo JSON
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'Musicas.json', true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        createMusicCards(data.Musicas); // Remova o "F" aqui
+    }
+};
+xhr.send();
+
+// Função para criar os cards das músicas
+function createMusicCards(musicas) {
+    const musicCardsContainer = document.getElementById('music-cards');
+    musicCardsContainer.innerHTML = ''; // Limpa o conteúdo antes de adicionar os novos cards
+    musicas.forEach(musica => {
+        const card = document.createElement('div');
+        card.classList.add('card-Musicas');
+        card.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${musica.Nome}</h5>
+        <p class="card-text">${musica.Trecho}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><strong>Código: ${musica.Codigo}</strong></li>
+        <li class="list-group-item">Interprete: ${musica.Interprete}</li>
+        <li class="list-group-item">Idioma: ${musica.Idioma}</li>
+    </ul>
+    </div>
+    </div>
+`;
+        musicCardsContainer.appendChild(card);
+    });
+}
+
+function search() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'Musicas.json', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const musicas = JSON.parse(xhr.responseText).Musicas;
+        const searchResult = musicas.filter(musica => {
+          return (
+            musica.Codigo.toString().includes(searchInput) ||
+            musica.Nome.toLowerCase().includes(searchInput) ||
+            musica.Interprete.toLowerCase().includes(searchInput)
+          );
+        });
+        createMusicCards(searchResult);
+      }
+    };
+    xhr.send();
+}
+
+function search_por_autor(autor) {
+    autor = autor.toLowerCase();
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'Musicas.json', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const musicas = JSON.parse(xhr.responseText).Musicas;
+        const searchResult = musicas.filter(musica => {
+          return (
+            musica.Codigo.toString().includes(autor) ||
+            musica.Nome.toLowerCase().includes(autor) ||
+            musica.Interprete.toLowerCase().includes(autor) ||
+            musica.Idioma.toLowerCase().includes(autor)
+          );
+        });
+        createMusicCards(searchResult);
+      }
+    };
+    xhr.send();
+}
+
+let isBRA = true;
+const image_pais = document.getElementById("img");
+
+  function toggleFunction() {
+    if (isBRA) {
+      search_por_autor('EUA');
+    } else {
+      search_por_autor('BRA');
+    }
+    isBRA = !isBRA;
+    updateButtonColor();
+  }
 
   function updateButtonColor() {
     const button = document.getElementById("toggleButton");
